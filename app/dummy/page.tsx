995802b2c92621f72e976c1d7ccc758e48e1523b
Umorenism <umorenismvic@gmail.com>
2024-08-25 06:52:08 -0700

    first commit

--- app/dummy/page.tsx
+++ app/dummy/page.tsx
@@ -1,13 +1,15 @@
-import React from "react";
-import BookingID from "../src/component/Bookingid/BookingID";
+import React, { Suspense } from "react";
+
 import Dummypage from "../src/component/Dummy/Dummypage";
 
-const page = () => {
+const Page = () => {
   return (
-    <div className="h-screen flex flex-col gap-12 justify-center w-full  items-center duration-300  dark:bg-black">
-      <Dummypage />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className="h-screen flex flex-col gap-12 justify-center w-full items-center duration-300 dark:bg-black">
+        <Dummypage />
+      </div>
+    </Suspense>
   );
 };
 
-export default page;
+export default Page;
